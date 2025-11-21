14deb4cd47851636f110b9a9d98572ce4a278f2b
jyoo <jyoo@student.42gyeongsan.kr>
2025-11-21 21:38:19 +0900

    chore : new add line

--- sub4/new_n_del.cpp
+++ sub4/new_n_del.cpp
@@ -16,7 +16,8 @@
 //delete &a;
 // 위는 오류가 일어남(heap이 아닌 영역을 해제하려 함)
 
-
+//새로운 단어: 스코프;
+// c++의 특이한 점; 변수 선언은 아무대서나 가능. 만약 중괄호 안에서 했다면 중괄호를 벗어나며 변수를 사라진다.
 int	main()
 {
 	int	arr_size;
@@ -27,7 +28,7 @@ int	main()
 	for (int i = 0; i < arr_size; i++)
 		std::cin >> list[i];
 	for (int i = 0; i < arr_size; i++)
-		std::cout << i << "th element of list : " << list[i] << std::endl;
+		std::cout << i << "th element of list : " << list[i] << std::endl; // 여기선 for 문 안에서 i 가 선언되었기에 이후 코드에서 i에 대해 활용할 수 ㅇ벗다.
 	delete[] list;
 	return 0;
 }
